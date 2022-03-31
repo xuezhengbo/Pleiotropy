31e8afdd179da580314ac93bafe9f7306e9b644b
xuezhengbo <95165515+xuezhengbo@users.noreply.github.com>
2022-03-31 19:01:40 +0800

    Update and rename qqman.R to manhattan_plot.R

--- code/manhattan_plot.R
+++ code/manhattan_plot.R
@@ -5,7 +5,7 @@ args <- commandArgs(trailingOnly=TRUE)
 path <- args[1]
 gwasResults <- fread(path, header=T, sep="\t")
          
-#计算膨胀系数
+#calculate lambda
 p_value <- gwasResults$P
 z <- qnorm(p_value/ 2)
 lambda <- round(median(z^2, na.rm = TRUE) / qchisq(0.5,1), 3)
@@ -18,14 +18,12 @@ print(lambda1000)
 color_set <- c("#4169E1","#87CEFA")
 #manhattan
 png(args[4])
-#tiff(args[4], width = 900, height = 300, res = 300, compression = "lzw", pointsize = 2)
 manhattan(gwasResults,chr = "CHR", bp = "POS", p = "P", snp = "SNP", col = color_set, ylim = c(0,35), cex.axis = 1, cex.lab = 1)
 while (!is.null(dev.list())) dev.off()
 
 #QQplot
-#png(args[5])
-#tiff(args[5], width = 400, height = 400, res = 300, compression = "lzw", pointsize = 1)
-#qq(gwasResults$P, cex.axis = 1, cex.lab = 1)
-#while (!is.null(dev.list())) dev.off()
+png(args[5])
+qq(gwasResults$P, cex.axis = 1, cex.lab = 1)
+while (!is.null(dev.list())) dev.off()
 
 
